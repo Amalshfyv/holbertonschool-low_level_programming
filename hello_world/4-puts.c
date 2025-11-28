53b5e9eb442bcf8f19b3e937433a30fffcbbb53e
Amalshfyv <amalshafiyev07@gmail.com>
2025-11-28 12:18:28 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - prints exactly "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
+
 
