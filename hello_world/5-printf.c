40681349d72d6b95dd3bacb9f060f9dbcfa763b7
Amalshfyv <amalshafiyev07@gmail.com>
2025-11-29 10:28:57 +0000

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,5 +1,9 @@
-#!/bin/bash
-#include <stdio.h>
+#include <stdio.>
+/**
+ * printf - just printing
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,");
