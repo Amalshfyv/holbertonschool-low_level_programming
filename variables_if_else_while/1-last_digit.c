cc32bed353d0e2eb17e97451a028a0d8f5bc5b18
Amalshfyv <amalshafiyev07@gmail.com>
2025-12-01 13:45:54 +0000

    question 1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/* main - enty point of the program */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n > 5)
+		printf("and is greater than 5\n");
+	else if (n == 0)
+		printf("and is 0\n");
+	else
+		printf("and is less than 6 and not 0");
+	return (0);
+}
