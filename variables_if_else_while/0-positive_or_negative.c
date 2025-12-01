e7ac84a0979e284c67d726d23e3e50fa057e6599
Amalshfyv <amalshafiyev07@gmail.com>
2025-12-01 13:20:54 +0000

    Readme

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/*  Code to generate a random number (do not touch)  */
+/* Your code starts here  */
 int main(void)
 {
     int n;
-
-    /* Code to generate a random number (do not touch) */
     srand(time(0));
-    n = rand() - RAND_MAX / 2;
-
-    /* Your code starts here */
+    n = rand() - RAND_MAX / 2;  
     printf("%d is ", n);
-
     if (n > 0)
         printf("positive\n");
     else if (n == 0)
         printf("zero\n");
     else
         printf("negative\n");
-
     return 0;
 }
