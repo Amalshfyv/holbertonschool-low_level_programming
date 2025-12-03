22d316f18a7d030283f1d988557514dc48a9f86b
Amalshfyv <amalshafiyev07@gmail.com>
2025-12-03 12:16:14 +0000

    aa

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a random number and states
+ *              whether it is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+**/
+	int main(void){
+	printf('\n', "_putchar");
+	}
