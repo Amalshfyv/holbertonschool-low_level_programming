9130f6beb577531b62e92aad2c745d892c78d7f7
Amalshfyv <amalshafiyev07@gmail.com>
2025-12-03 11:07:37 +0000

    a

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a random number and states
+ *              whether it is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+**/
+        int main(void){
+
+                char c = 'a';
+		char d = 'A';
+
+                for(; c <= 'z'; c++)
+                        putchar(c);
+              	for(; d <= 'Z'; d++)
+			putchar(d);
+                return (0);
+        }
+~            
