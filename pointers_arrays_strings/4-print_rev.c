841b79fe83e8631054d9b55dfc94d742e04f3a10
Amalshfyv <amalshafiyev07@gmail.com>
2025-12-10 11:49:52 +0000

    a

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+/**
+ *print_rev - print a string in reverse
+ *@s: string
+ */
+#include <unistd.h>
+#include "main.h"
+void print_rev(char *s)
+{
+int count = 0;
+int i;
+while (s[count] != '\0')
+{
+	count++;
+}
+for (i = count - 1; i >= 0; i--)
+{
+	write(1, &s[i], 1);
+}
+write(1, "\n", 1);
+}
