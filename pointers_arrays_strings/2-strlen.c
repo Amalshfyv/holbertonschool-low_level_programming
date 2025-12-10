ca3b039e24c8ad45af4a197945836e9f03a78157
Amalshfyv <amalshafiyev07@gmail.com>
2025-12-10 11:35:37 +0000

    a

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+/**
+ *_strlen - calculate the len of string
+ *@s: string
+ *Return: len
+ */
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; *s != '\0'; s++)
+		len++;
+	return (len);
+}
