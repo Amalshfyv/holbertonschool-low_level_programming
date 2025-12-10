4068b48b89476e526c6e296fee0d0eb475a90bcc
Amalshfyv <amalshafiyev07@gmail.com>
2025-12-10 11:24:19 +0000

    a

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,10 +1,13 @@
-#include "main.h"
 /**
  *swap_int - updates the value to 98
  *@a: input
  *@b: input
  *
  */
-void swap_int(int *a, int *b){
-	swap(&a, &b);
+#include "main.h"
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
