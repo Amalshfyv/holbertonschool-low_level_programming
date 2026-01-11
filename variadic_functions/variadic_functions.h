08973ddb785bebcbaa2241a7d7b4cca4e74ab5cc
Amal Shafiyev <amalshafiyev07@gmail.com>
2026-01-11 11:31:21 +0000

    Sum them all

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef VARIADIC
+#define VARIADIC
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#include <stdarg.h>
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+#endif
