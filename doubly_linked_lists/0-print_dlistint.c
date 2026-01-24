d9b956564148607797526b81325e87df0d355bb3
Amalshfyv <amalshafiyev07@gmail.com>
2026-01-24 14:31:12 +0000

    a

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,19 @@
- 
+/**
+ * print_dlistint - function with one arguments
+ * @h: linked list pointer
+ * Return: number of nodes
+ */
+#include <stdio.h>
+#include "lists.h"
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+} 
